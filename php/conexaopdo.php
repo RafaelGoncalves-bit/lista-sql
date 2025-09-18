6414afda56a3242fa1bb1d5495fddb52a1d7c402
Rafael <ltlrafaelg@gmail.com>
2025-09-18 19:56:46 -0300

    modify conexao

---
+++ php/conexaopdo.php
@@ -0,0 +1,15 @@
+<?php
+
+//conexao.php
+$host = "127.0.0.1";
+$dbname = "empresa";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname charset=utf8", $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
